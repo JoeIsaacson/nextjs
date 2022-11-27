b66de8bb92e5f9bf92a1bf3fa606081a0c5cad05
Joey Isaacson <joey.a.isaacson@gmail.com>
2022-11-27 07:24:45 +0000

    whoooops

--- pages/uber.js
+++ pages/uber.js
@@ -1,76 +1,12 @@
 function Uber() {
-    return 
     
-    <div className={styles.container}>
-
-      <main className={styles.main}>
-
-        <h1 className={styles.title}>
-          Hey, im Joey
-        </h1>
-
-        <h6 className={styles.description}>design manager. leader. 🌐 internet money guy.</h6>
-
-        <p className={styles.description}>
-        Leading several design teams at <a className={styles.textLink} href="https://www.coinbase.com/" target="blank">Coinbase</a> as a design manager, focused on helping people earn interest, send and spend their crypto. Its been a wild ride. Before that, I was leading the marketplace dynamics team at <a className={styles.textLink} href='https://www.uberfreight.com/carrier/' target="blank">Uber Freight</a>. Before that - man I was a snotty nosed designer working at a <a className={styles.textLink} href="https://www.linkedin.com/in/joeisaacson/" target="blank">few small start ups</a>.
-        </p>
-        
-        <img alt ="" src="/context_1.svg"></img>
-        <img alt ="" src="/context_2.svg"></img>
-        <img alt ="" src="/context_3.svg"></img>
-        <img alt ="" src="/context_4.svg"></img>
-        <img alt ="" src="/context_5.svg"></img>
-        <img alt ="" src="/context_6.svg"></img>
-        <img alt ="" src="/context_7.svg"></img>
-        <img alt ="" src="/context_8.svg"></img>
-        <img alt ="" src="/context_9.svg"></img>
-
-        <img alt ="" src="/p1_1.svg"></img>
-        <img alt ="" src="/p1_2.svg"></img>
-        <img alt ="" src="/p1_3.svg"></img>
-        <img alt ="" src="/p1_4.svg"></img>
-        <img alt ="" src="/p1_5.svg"></img>
-        <img alt ="" src="/p1_6.svg"></img>
-        <img alt ="" src="/p1_7.svg"></img>
-        <img alt ="" src="/p1_8.svg"></img>
-        <img alt ="" src="/p1_9.svg"></img>
-        <img alt ="" src="/p1_10.svg"></img>
-        <img alt ="" src="/p1_11.svg"></img>
-        <img alt ="" src="/p1_12.svg"></img>
-
-        <img alt ="" src="/p2_1.svg"></img>
-        <img alt ="" src="/p2_2.svg"></img>
-        <img alt ="" src="/p2_3.svg"></img>
-        <img alt ="" src="/p2_4.svg"></img>
-        <img alt ="" src="/p2_5.svg"></img>
-        <img alt ="" src="/p2_6.svg"></img>
-        <img alt ="" src="/p2_7.svg"></img>
-        <img alt ="" src="/p2_8.svg"></img>
-        <img alt ="" src="/p2_9.svg"></img>
-        <img alt ="" src="/p2_10.svg"></img>
-        <img alt ="" src="/p2_11.svg"></img>
-        <img alt ="" src="/p2_13.svg"></img>
-        <img alt ="" src="/p2_14.svg"></img>
-        <img alt ="" src="/p2_15.svg"></img>
-        <img alt ="" src="/p2_16.svg"></img>
-        <img alt ="" src="/p2_17.svg"></img>
-        <img alt ="" src="/p2_18.svg"></img>
-        <img alt ="" src="/p2_19.svg"></img>
-        <img alt ="" src="/p2_20.svg"></img>
-        <img alt ="" src="/p2_21.svg"></img>
-        <img alt ="" src="/p2_22.svg"></img>
-        <img alt ="" src="/p2_23.svg"></img>
-        <img alt ="" src="/p2_24.svg"></img>
-
-        <h3 className={styles.description}>Coinbase → </h3>
-        <h3 className={styles.description}>Uber Freight →</h3>
-
-      </main>
-
+    console.log("new work page!");
+  
+    return (<div className={styles.container}>
       <footer className={styles.footer}>
        designed and built by <a className={styles.textLink} href='https://github.com/JoeIsaacson' target="blank">joey isaacson</a>
       </footer>
-    </div>
+    </div>);
   }
   
   export default Uber;
