9fd2b4169076e6b385394e9ca13f870ddfd0fa4f
Joey Isaacson <joey.a.isaacson@gmail.com>
2022-11-27 07:02:47 +0000

    debuggin

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
+
+  console.log("is this even working");
 }
 
 export default MyApp;
--- pages/_uber.js
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css';
-
-function whatever({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default whatever;
