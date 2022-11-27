4ecd98d253c6abb97a63bff574603d3d58e14ee2
Joey Isaacson <joey.a.isaacson@gmail.com>
2022-11-27 02:21:01 +0000

    straight to prod

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
 
         <h1 className={styles.title}>
-          Hello
+          Hey, i'm Joey
         </h1>
 
         <h5 className={styles.description}>
